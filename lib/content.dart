c1afc6777593e946fc43cae48ada794203f5eb92
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2020-08-29 20:26:16 +0200

    Added footnote parsing to timetable parsing.

--- lib/content.dart
+++ lib/content.dart
@@ -19,6 +19,37 @@ class Content {
   String toString() {
     return cells.toString();
   }
+
+  void combine(Content other) {
+    for (int y = 0; y < cells.length; y++) {
+      for (int x = 0; x < cells[y].length; x++) {
+        var myCell = cells[y][x];
+        var otherCell = other.cells[y][x];
+        // If current cell is empty set cell to other
+        if (myCell.isEmpty()) {
+          setCell(y, x, otherCell);
+        }
+        // If other cell is not empty set cell to current
+        if (!otherCell.isEmpty()) {
+          setCell(y, x, otherCell);
+        }
+      }
+    }
+  }
+}
+
+class Footnote {
+  String teacher;
+  String subject;
+  String room;
+  String schoolClass;
+  String schoolWeek;
+  String text;
+
+  @override
+  String toString() {
+    return "{Footnote teacher:${teacher}, subject:${subject}, room:${room}}";
+  }
 }
 
 class Cell {
@@ -30,10 +61,16 @@ class Cell {
   String teacher = "---";
   String originalTeacher = "---";
   String text = "---";
+  String footNoteTextId = "";
+  List<Footnote> footnotes;
   bool isDropped = false;
   bool isDoubleClass = false;
 
   String toString() {
     return "{Cell isDropped : ${isDropped}, subject : ${subject}, room : ${room}}";
+  }
+
+  bool isEmpty() {
+    return subject == "---" && room == "---" && teacher == "---" && isDropped == false;
   }
 }
--- lib/parse.dart
+++ lib/parse.dart
@@ -17,9 +17,15 @@ Future<void> initiate(course, Content content, List<String> subjects) async {
   var client = Client();
   var weekDay = DateTime.now().weekday;
 
+  print("Parsing main time table");
   await fillTimeTable(course, TIMETABLE_LINK_BASE, client, content, subjects);
-  await fillTimeTable("11K", TIMETABLE_LINK_BASE, client, content, subjects);
+  print("Parsing course only time table");
+  var courseTimeTableContent = new Content(6, 10);
+  await fillTimeTable("11K", TIMETABLE_LINK_BASE, client, courseTimeTableContent, subjects);
+  print("Combining both tables");
+  content.combine(courseTimeTableContent);
 
+  print("Parsing submission plan");
   List<HashMap<String, String>> plan = await getCourseSubsitutionPlan(course, SUBSTITUTION_LINK_BASE, client);
   List<HashMap<String, String>> coursePlan = await getCourseSubsitutionPlan("11K", SUBSTITUTION_LINK_BASE, client);
   plan.addAll(coursePlan);
@@ -48,7 +54,6 @@ Future<void> initiate(course, Content content, List<String> subjects) async {
       // Hour range (5-6)
       var hourStart = int.parse(hours[0]);
       var hourEnd = int.parse(hours[1]);
-      print("${hourStart} ${hourEnd}");
       for (var i = hourStart; i < hourEnd + 1; i++) {
         content.setCell(i-1, min(weekDay, 5), cell);
       }
@@ -65,16 +70,192 @@ Future<void> fillTimeTable(String course, String linkBase, client, Content conte
 
   var document = parse(response.body);
 
-  // Find all tables with attr rules
-  List<dom.Element> tables = document.getElementsByTagName("table");
-  for (int i = 0; i < tables.length; i++) {
-    if (!tables[i].attributes.containsKey("rules")) {
-      tables.removeAt(i);
+  // Find all elements with attr rules
+  List<dom.Element> tables = new List<dom.Element>();
+  List<dom.Element> elements = document.getElementsByTagName("body")[0].children[0].children;
+  for (int i = 0; i < elements.length; i++) {
+    if (elements[i].attributes.containsKey("rules")) {
+      tables.add(elements[i]);
     }
   }
   var mainTimeTable = tables[0];
   var footnoteTable = tables[1];
+  var footnoteMap = parseFootnoteTable(subjects, footnoteTable);
+  parseMainTimeTable(content, subjects, mainTimeTable, footnoteMap);
+}
 
+
+class Area {
+  int columnStart;
+  int columnEnd;
+  int rowStart;
+  int rowEnd;
+
+  @override
+  String toString() {
+    return "{Area columnStart:${columnStart},columnEnd:${columnEnd}, rowStart:${rowStart}, rowEnd:${rowEnd}}";
+  }
+}
+
+
+HashMap<String,List<Footnote>> parseFootnoteTable(List<String> subjects, dom.Element footnoteTable) {
+  List<dom.Element> rows = footnoteTable.children[0].children;
+
+  List<String> headerColumnsText = new List<String>();
+  HashMap<String, List<int>> headerColumnStringIndexMap = new HashMap<String, List<int>>();
+  HashMap<int, List<String>> columnData = new HashMap<int, List<String>>();
+
+  // Find column header text
+  var headerColumns = rows[0].children;
+  for (var i = 0; i < headerColumns.length; i++) {
+    var headerColumn = headerColumns[i];
+    headerColumnsText.add(strip(headerColumn.text));
+    columnData[i] = new List<String>();
+  }
+
+  // Map column text to column indexes
+  for (var i = 0; i < headerColumnsText.length; i++) {
+    var headerColumnText = headerColumnsText[i];
+    if (headerColumnStringIndexMap.containsKey(headerColumnText)) {
+      headerColumnStringIndexMap[headerColumnText].add(i);
+    } else {
+      headerColumnStringIndexMap[headerColumnText] = [i];
+    }
+  }
+  rows.removeAt(0);  // remove header from rows
+
+  // Convert format to columns first instead of rows
+  for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    var row = rows[rowIndex];
+    var columns = row.children;
+    for (var i = 0; i < columns.length; i++) {
+      columnData[i].add(strip(columns[i].text).replaceAll("\n", ""));
+    }
+  }
+
+  // Find footnote areas
+  List<Area> areas = new List<Area>();
+  List<String> areaFootnotes = new List<String>();
+
+  // Loop over all columns with the header Nr.
+  var nrList = headerColumnStringIndexMap["Nr."];
+  for (var i = 0; i < nrList.length; i++) {
+    var columnIndex = nrList[i];
+    var column = columnData[columnIndex];
+    var currentFootnoteIndex = column[0];
+
+    // Get the columnStart and End
+    var columnStart = columnIndex;
+    int columnEnd;
+    if (i >= nrList.length-1) {
+      columnEnd = columnData.length-1;
+    } else {
+      columnEnd = nrList[i+1]-1;
+    }
+
+    // Setup first area
+    var currentArea = new Area();
+    currentArea.rowStart = 0;
+    currentArea.columnStart = columnStart;
+    currentArea.columnEnd = columnEnd;
+    var lastJ = 0;
+
+    // Loop over one column with the header Nr.
+    for (var j = 0; j < column.length; j++) {
+      var currentValue = column[j];
+      lastJ = j;
+
+      if (currentValue == " " || currentValue == currentFootnoteIndex) {  // No start of a new area
+        continue;
+      } else {  // Start of new area
+        // Add old area to list
+        currentArea.rowEnd = j-1;
+        areaFootnotes.add(currentFootnoteIndex);
+        areas.add(currentArea);
+
+        // Init new area
+        currentFootnoteIndex = column[j];
+        currentArea = new Area();
+        currentArea.rowStart = j;
+        currentArea.columnStart = columnStart;
+        currentArea.columnEnd = columnEnd;
+      }
+    }
+    // Add last area to list
+    currentArea.rowEnd = lastJ;
+    areaFootnotes.add(currentFootnoteIndex);
+    areas.add(currentArea);
+  }
+
+
+  // Parse footnote areas
+  var lastFootnoteKey = "1)";
+  HashMap<String,List<Footnote>> footnotesMap = new HashMap<String,List<Footnote>>();
+  for (var i = 0; i < areas.length; i++) {
+    var footnoteKey = areaFootnotes[i];
+    var area = areas[i];
+    var relevantColumns = new List<List<String>>();
+
+    // Get relevant columns within area
+    var columnTextList = new List<String>();
+    for (var i = area.columnStart; i < area.columnEnd + 1; i++) {
+      var relevantColumn = new List<String>();
+      columnTextList.add(headerColumnsText[i]);
+      for (var j = area.rowStart; j < area.rowEnd + 1; j++) {
+        relevantColumn.add(columnData[i][j]);
+      }
+      relevantColumns.add(relevantColumn);
+    }
+
+    // Init footnotes
+    var footnotes = new List<Footnote>();
+    for (var i = 0; i < area.rowEnd-area.rowStart + 1; i++) {
+      footnotes.add(new Footnote());
+    }
+
+    // Loop over all columns
+    for (var columnIndex = 0; columnIndex < relevantColumns.length; columnIndex++) {
+      // Loop over all rows
+      for (var rowIndex = 0; rowIndex < relevantColumns[columnIndex].length; rowIndex++) {
+        var value = relevantColumns[columnIndex][rowIndex];
+
+        // Switch on current column header and set footnotes
+        switch (columnTextList[columnIndex]) {
+          case "Le.,Fa.,Rm.":
+            var splitValue = value.split(",");
+            if (splitValue.length >= 3) {
+              footnotes[rowIndex].teacher = splitValue[0];
+              footnotes[rowIndex].subject = splitValue[1];
+              footnotes[rowIndex].room = splitValue[2];
+            }
+            break;
+          case "Kla":
+            footnotes[rowIndex].schoolClass = value;
+            break;
+          case "Schulwoche":
+            footnotes[rowIndex].schoolWeek = value;
+            break;
+          case "Text":
+            footnotes[rowIndex].text = value;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    // Append footnote to last footnote if the current footnote key is " "
+    if (footnoteKey == " ") {
+      footnotesMap[lastFootnoteKey].addAll(footnotes);
+    } else {
+      footnotesMap[footnoteKey] = footnotes;
+      lastFootnoteKey = footnoteKey;
+    }
+  }
+  return footnotesMap;
+}
+
+void parseMainTimeTable(Content content, List<String> subjects, dom.Element mainTimeTable, HashMap<String,List<Footnote>> footnoteMap) {
   List<dom.Element> rows = mainTimeTable.children[0].children;
   rows.removeAt(0);
 
@@ -87,12 +268,12 @@ Future<void> fillTimeTable(String course, String linkBase, client, Content conte
     }
     for (var x = 0; x < 6; x++) {
       if (x == 0) {
-        parseOneCell(columns[x], x, y, content, subjects);
+        parseOneCell(columns[x], x, y, content, subjects, footnoteMap);
         tableX++;
       } else {
         var doParseCell = true;
         if (y != 0) {
-          var contentY = (y/2).floor();
+          var contentY = (y / 2).floor();
           if (contentY >= content.cells.length) {
             continue;
           }
@@ -100,10 +281,9 @@ Future<void> fillTimeTable(String course, String linkBase, client, Content conte
           if (isDoubleClass) {
             doParseCell = false;
           }
-          print("${contentY} ${x} ${doParseCell} ${tableX}");
         }
         if (doParseCell) {
-          parseOneCell(columns[tableX], x, y, content, subjects);
+          parseOneCell(columns[tableX], x, y, content, subjects, footnoteMap);
           tableX++;
         }
       }
@@ -111,9 +291,9 @@ Future<void> fillTimeTable(String course, String linkBase, client, Content conte
   }
 }
 
-void parseOneCell(dom.Element cellDom, int x, int y, Content content, List<String> subjects) {
+void parseOneCell(dom.Element cellDom, int x, int y, Content content, List<String> subjects, HashMap<String,List<Footnote>> footnoteMap) {
   var cell = new Cell();
-  // sidebar
+  // Sidebar
   if (x == 0) {
     return;
   }
@@ -128,6 +308,29 @@ void parseOneCell(dom.Element cellDom, int x, int y, Content content, List<Strin
     cell.teacher = strip(teacherAndRoom[0].text);
     cell.room = strip(teacherAndRoom[1].text);
     cell.subject = strip(subjectAndFootnote[0].text);
+
+    // Check if footnote exists
+    if (subjectAndFootnote.length > 1) {
+      // Get footnotes
+      var footnoteKey = strip(subjectAndFootnote[1].text);
+      var footnotes = footnoteMap[footnoteKey];
+
+      // Filter out footnotes that don't matter to the user
+      var requiredFootnotes = new List<Footnote>();
+      for (var footnote in footnotes) {
+        if (subjects.contains(footnote.subject)) {
+          requiredFootnotes.add(footnote);
+        }
+      }
+
+      // If only one required footnote set the subject room and teacher
+      if (requiredFootnotes.length == 1) {
+        cell.subject = requiredFootnotes[0].subject;
+        cell.room = requiredFootnotes[0].room;
+        cell.teacher = requiredFootnotes[0].teacher;
+      }
+      cell.footnotes = requiredFootnotes;  // Set footnotes of cell
+    }
   }
 
   if (subjects.contains(cell.subject)) {  // If user dose not have that subject skip that class
@@ -170,8 +373,7 @@ Future<List<HashMap<String, String>>> getCourseSubsitutionPlan(
     "Entfall"
   ];
   rows.removeAt(0);
-  List<HashMap<String, String>> subsituions =
-      new List<HashMap<String, String>>();
+  List<HashMap<String, String>> subsituions = new List<HashMap<String, String>>();
 
   for (var row in rows) {
     HashMap<String, String> substituion = new HashMap<String, String>();
