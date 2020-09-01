9ad18d6cbcf84cca17e8fecb252bd08b54e15448
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2020-09-01 20:48:31 +0200

    Added data necessary structures for the profile system.

---
+++ lib/Profile.dart
@@ -0,0 +1,25 @@
+
+class Profile {
+  String schoolGrade = "11";
+  String subSchoolClass = "e";
+  List<String> subjects = [];
+
+  Map getJsonData() {
+    Map jsonData = {
+      "schoolGrade" : schoolGrade,
+      "subSchoolClass" : subSchoolClass,
+      "subjects" : subjects
+    };
+    return jsonData;
+  }
+
+  static Profile fromJsonData(dynamic jsonData) {
+    Profile newProfile = new Profile();
+    newProfile.schoolGrade = jsonData["schoolGrade"];
+    newProfile.subSchoolClass = jsonData["subSchoolClass"];
+    for (String subject in jsonData["subjects"]) {
+      newProfile.subjects.add(subject);
+    }
+    return newProfile;
+  }
+}
--- lib/shared_state.dart
+++ lib/shared_state.dart
@@ -1,40 +1,49 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stundenplan/constants.dart';
 import 'package:stundenplan/theme.dart';
+import 'dart:convert';
+
+import 'Profile.dart';
 
 class SharedState {
   SharedPreferences preferences;
 
   Theme theme = darkTheme;
-  String schoolGrade = "11";
-  String subSchoolClass = "e";
   int height = Constants.defaultHeight;
-  List<String> subjects = [];
+  int currentProfileIndex = 0;
+  List<Profile> profiles = [Profile()];
 
   SharedState(this.preferences);
 
   void saveState() {
     preferences.setString("theme", theme.themeName);
-    preferences.setString("schoolGrade", schoolGrade);
-    preferences.setString("subSchoolClass", subSchoolClass);
+    preferences.setInt("currentProfileIndex", currentProfileIndex);
+    List<Map> jsonProfileData = new List<Map>();
+    for (Profile profile in profiles) {
+      jsonProfileData.add(profile.getJsonData());
+    }
+    preferences.setString("jsonProfileData", jsonEncode(jsonProfileData));
     preferences.setInt("height", height);
-    preferences.setStringList("subjects", subjects);
   }
 
   bool loadStateAndCheckIfFirstTime() {
     setThemeFromThemeName(preferences.getString("theme") ?? "dark");
-    subSchoolClass = preferences.getString("subSchoolClass") ?? "e";
-    subjects = preferences.getStringList("subjects") ?? [];
-    height = preferences.getInt("height") ?? Constants.defaultHeight;
+    height = preferences.getInt("height");
 
     // If first time using app
-    schoolGrade = preferences.getString("schoolGrade");
-    if (schoolGrade == null) {
-      schoolGrade = "11";
+    if (height == null) {
+      height = Constants.defaultHeight;
       return true;
+    }
+
+    profiles = [];
+    var jsonProfilesData = jsonDecode(preferences.getString("jsonProfileData"));
+    for (var jsonProfileData in jsonProfilesData) {
+      profiles.add(Profile.fromJsonData(jsonProfileData));
     }
     return false;
   }
+
 
   void setThemeFromThemeName(String themeName) {
     this.theme = Theme.getThemeFromThemeName(themeName);
@@ -49,6 +58,34 @@ class SharedState {
       }
     }
     return Constants.alwaysDefaultSubjects;
+  }
+
+  Profile get currentProfile {
+    return profiles[currentProfileIndex];
+  }
+
+  String get schoolGrade {
+    return currentProfile.schoolGrade;
+  }
+
+  set schoolGrade(String schoolGrade) {
+    currentProfile.schoolGrade = schoolGrade;
+  }
+
+  String get subSchoolClass {
+    return currentProfile.subSchoolClass;
+  }
+
+  set subSchoolClass(String subSchoolClass) {
+    currentProfile.subSchoolClass = subSchoolClass;
+  }
+
+  List<String> get subjects {
+    return currentProfile.subjects;
+  }
+
+  set subjects(List<String> subjects) {
+    currentProfile.subjects = subjects;
   }
 }
 
