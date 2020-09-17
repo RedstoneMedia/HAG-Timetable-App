d2c715a0e555c5c40862325f2f9fa7344bd6fa1f
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2020-09-17 17:44:58 +0200

    Fixed subst date parsing regex 2

--- lib/parsing/parse_subsitution_plan.dart
+++ lib/parsing/parse_subsitution_plan.dart
@@ -69,7 +69,7 @@ Future<Tuple2<List<HashMap<String, String>>, int>> getCourseSubstitutionPlan(Str
 
   // Get weekday for that substitute table
   String headerText = strip(document.getElementsByTagName("body")[0].children[0].children[0].children[2].text.replaceAll("  ", "/"));
-  var regexp = RegExp(r"^\w+\/(?<day>\d+).(?<month>\d).");
+  var regexp = RegExp(r"^\w+\/(?<day>\d+).(?<month>\d+).");
   RegExpMatch match = regexp.firstMatch(headerText);
   print("${DateTime.now().year}.${int.parse(match.namedGroup("month"))}.${int.parse(match.namedGroup("day"))}");
   var substituteWeekday = DateTime(DateTime.now().year, int.parse(match.namedGroup("month")), int.parse(match.namedGroup("day"))).weekday;
