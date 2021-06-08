2882d270a3bc2905b8f38affa112060c697066eb
GreenOpposite <henriette3d@gmail.com>
2021-06-08 19:02:22 +0200

    Added base_intro_screen widget

---
+++ lib/widgets/base_intro_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:stundenplan/shared_state.dart';
+
+class BaseIntroScreen extends StatelessWidget {
+  const BaseIntroScreen(
+      {required this.sharedState,
+      required this.title,
+      required this.subtitle,
+      required this.child,
+      required this.onPressed,
+      this.buttonText = "Weiter"});
+
+  final SharedState sharedState;
+  final String title;
+  final String subtitle;
+  final String buttonText;
+  final Widget child;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: sharedState.theme.backgroundColor,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                          color: sharedState.theme.textColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40.0),
+                    ),
+                  ),
+                  Text(
+                    subtitle,
+                    style: TextStyle(
+                        color: sharedState.theme.textColor, fontSize: 18.0),
+                  ),
+                ],
+              ),
+              child,
+              ElevatedButton(
+                onPressed: onPressed,
+                style: ButtonStyle(
+                  shape:  MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                      )
+                  ),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    sharedState.theme.subjectColor,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                  child: Text(
+                    buttonText,
+                    style: TextStyle(
+                        color: sharedState.theme.textColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
