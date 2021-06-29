be443519500666bc021d73507b4a19b980defb52
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2021-06-29 16:55:31 +0200

    Added css transition and easier local setup

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+function fixAnimationOnPageLoad() {
+  var c = document.getElementsByTagName('content-center')[0];
+  function addAnim() {
+      c.classList.add('animated')
+      // Remove the listener, no longer needed
+      c.removeEventListener('mouseover', addAnim);
+  };
+  // Listen to mouseover for the container
+  c.addEventListener('mouseover', addAnim);
+}
+
 particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
+  let el = document.querySelector(".particles-js-canvas-el"); 
+  el.setAttribute("width", window.innerWidth.toString());
 });
+fixAnimationOnPageLoad();
