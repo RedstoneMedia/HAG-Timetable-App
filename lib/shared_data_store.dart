6ba5884c15610697c6dc3af199737763a3a0fc5d
RedstoneMedia <34373974+RedstoneMedia@users.noreply.github.com>
2022-02-21 22:33:28 +0100

    Implemented basic functionality for shared_data_store

--- lib/shared_data_store.dart
+++ lib/shared_data_store.dart
@@ -7,10 +7,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tuple/tuple.dart';
 
 class SharedDataStore {
-  NearbyService nearbyService = NearbyService();
-  Map<String, Tuple2<DateTime, dynamic>> data = {};
+  final NearbyService nearbyService = NearbyService();
+  final Map<String, Tuple2<DateTime, dynamic>> data = {};
   bool running = false;
   SharedPreferences preferences;
+  final List<String> connectedDeviceIds = [];
 
   SharedDataStore(this.preferences);
 
@@ -44,6 +45,29 @@ class SharedDataStore {
     return data[propertyName]?.item2;
   }
 
+  Future<void> syncLoop() async {
+      while (running) {
+        // Periodically broadcast all properties and their timestamp, so peers can request a value if they don't have that value, or have a older version
+        final syncData = <String, String>{};
+        for (final entry in data.entries) {
+          syncData[entry.key] = entry.value.item1.toIso8601String();
+        }
+        await broadcastMessage(jsonEncode({"type": "sync", "data": syncData}));
+        await Future.delayed(const Duration(milliseconds: 500));
+      }
+  }
+
+  Future<void> broadcastMessage(String message) async {
+    final futures = <Future<dynamic>>[];
+    for (final peerId in connectedDeviceIds) {
+      final result = nearbyService.sendMessage(peerId, message);
+      if (result.runtimeType == Future && result != null) {
+        futures.add(result as Future);
+      }
+    }
+    await Future.wait(futures);
+  }
+
   Future<void> start() async {
     if (running) return;
     running = true;
@@ -63,22 +87,60 @@ class SharedDataStore {
 
     nearbyService.stateChangedSubscription(callback: (devicesList) {
       for (final device in devicesList) {
+        if (device.state == SessionState.notConnected && !connectedDeviceIds.contains(device.deviceId)) {
+          nearbyService.invitePeer(deviceID: device.deviceId, deviceName: device.deviceName);
+        } else if (device.state == SessionState.notConnected && connectedDeviceIds.contains(device.deviceId)) {
+          connectedDeviceIds.remove(device.deviceId);
+        } else if (device.state == SessionState.connected && !connectedDeviceIds.contains(device.deviceId)) {
+          connectedDeviceIds.add(device.deviceId);
+        }
         log("deviceId: ${device.deviceId} | deviceName: ${device.deviceName} | state: ${device.state}", name: "Shared-Data-Store");
       }
     });
 
-    nearbyService.dataReceivedSubscription(callback: (data) {
-      final jsonData = jsonDecode(data as String) as Map<String, dynamic>;
-      log(jsonData.toString(), name: "Shared-Data-Store");
+    nearbyService.dataReceivedSubscription(callback: (receivedData) async {
+      final jsonData = jsonDecode(receivedData as String) as Map<String, dynamic>;
+      log(jsonData.toString(), name: "Shared-Data-Store"); //
       final fromDeviceId = jsonData["deviceId"] as String;
       final messageData = jsonData["message"] as Map<String, dynamic>;
-      final isResponse = messageData["isResponse"] as bool;
-      if (isResponse) {
-        // TODO: Implement receiving and updating data, but ignoring old peer data
-      } else {
-        nearbyService.sendMessage(fromDeviceId, jsonEncode(data));
+      final messageType = messageData["type"] as String;
+      switch (messageType) {
+        case "sync":
+          final syncData = messageData["data"] as Map<String, String>;
+          final newKeys = <String>[];
+          for (final entry in syncData.entries) {
+            if (!data.containsKey(entry.key)) {
+              newKeys.add(entry.key);
+            } else if (DateTime.parse(entry.value).isAfter(data[entry.key]!.item1)) {
+              newKeys.add(entry.key);
+            }
+          }
+          if (newKeys.isNotEmpty) {
+            nearbyService.sendMessage(fromDeviceId, jsonEncode({"type" : "get", "keys": newKeys}));
+          }
+          break;
+        case "get":
+          final requestedKeys = messageData["keys"] as List<String>; // TODO: Only send requested keys and not everything
+          await nearbyService.sendMessage(fromDeviceId, jsonEncode({"type" : "data", "data": getJsonData()}));
+          break;
+        case "data":
+          final peerData = messageData["data"] as Map<String, dynamic>;
+          // TODO: Maybe do some validation of the timestamp (and that the data has not been tampered with (HMAC maybe)) (at least for relayed data), since a malicious peer could spread bad data easily
+          for (final property in peerData.entries) {
+            final valueList = property.value as List;
+            final timestamp = DateTime.parse(valueList[0] as String);
+            if (!data.containsKey(property.key)) {
+              data[property.key] = Tuple2(timestamp, valueList[1]);
+              continue;
+            }
+            if (timestamp.isAfter(data[property.key]!.item1)) {
+              data[property.key] = Tuple2(timestamp, valueList[1]);
+            }
+          }
+          break;
       }
     });
+    unawaited(syncLoop());
     log("Started", name: "Shared-Data-Store");
   }
 
